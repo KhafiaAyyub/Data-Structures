d9bedf283b288a380a1a74a549c2f9c48ac99509
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-01-26 21:53:16 +0530

    commit

--- Stacks/NextSmallerElementToRight.java
+++ Stacks/NextSmallerElementToRight.java
@@ -1,1 +1,17 @@
+ //nse to right -> loop from r to l
+        int[] rb = new int[arr.length]; //nse index on right (rb-right boundary)
+        Stack<Integer> st = new Stack<>();
+        st.push(arr.length-1);
+        rb[arr.length - 1] = arr.length;
 
+        for(int i=arr.length- 2;i>=0;i--){
+            while(st.size() > 0 && arr[i] < arr[st.peek()]){
+                st.pop();
+            }
+            if(st.size() == 0){
+                rb[i] = arr.length;
+            }else{
+                rb[i] = st.peek();
+            }
+            st.push(i);
+        }
