4331682396bbea717df8f3b401b3d3b349d5bbbb
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-01-26 21:55:28 +0530

    commit

--- Stacks/NextSmallerElementToLeft.java
+++ Stacks/NextSmallerElementToLeft.java
@@ -1,1 +1,18 @@
 
+        //nse index to left
+        int[] lb = new int[arr.length];
+        st = new Stack<>();
+        st.push(0);
+        lb[0] = -1;
+
+        for(int i=1;i<arr.length;i++){
+            while(st.size() > 0 && arr[i] < arr[st.peek()]){
+                st.pop();
+            }
+            if(st.size() == 0){
+                lb[i] = -1;
+            }else{
+                lb[i] = st.peek();
+            }
+            st.push(i);
+        }
