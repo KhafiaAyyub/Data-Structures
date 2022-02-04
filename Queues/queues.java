63fe5b5ca08864760bddedf2422b29f3a9fd9974
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-02-04 23:29:05 +0530

    commit

--- Queues/queues.java
+++ Queues/queues.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class QueuesIntro {
+    public static void main(String[] args){
+        Queue<Integer> que = new ArrayDeque<>();
+        
+        que.add(10);
+        System.out.println(que); //10
+        que.add(20);
+        System.out.println(que);//10,20
+        que.add(30);
+        System.out.println(que);//10,20,30
+
+        System.out.println(que.peek());//10
+        System.out.println(que);//10,20,30
+
+        System.out.println(que.remove());//10
+        System.out.println(que);//20,30
+        
+    }
+}
