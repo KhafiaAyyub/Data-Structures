cafc1d5238a99250a1cd01bf1bebe91e8dd30681
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-02-20 22:51:40 +0530

    commit

--- GenericTrees/GenericTrees.java
+++ GenericTrees/GenericTrees.java
@@ -1,1 +1,12 @@
+import java.util.*;
 
+public class GTIntro {
+    private class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void main(String[]args) {
+        Node root;
+    }
+}
