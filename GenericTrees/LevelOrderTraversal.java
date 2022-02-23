d69225c4915dcb7788ad9b2f70b84f095ce1c0fa
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-02-23 20:29:11 +0530

    commit

--- GenericTrees/LevelOrderTraversal.java
+++ GenericTrees/LevelOrderTraversal.java
@@ -1,1 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class LevelOrderTraversal {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static void levelOrder(Node node){
+    Queue<Node> q =new ArrayDeque<>();
+    q.add(node);
+    
+    while(q.size() != 0){
+        node = q.remove();
+        System.out.print(node.data + " ");
+        for(Node child:node.children){
+            q.add(child);
+        }
+    }
+    System.out.print(".");
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    levelOrder(root);
+  }
+
+}
 
