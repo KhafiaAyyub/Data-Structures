02deada7e11f6a5ff3fb0d29c3078510b9dfc7d8
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-02-23 20:22:34 +0530

    commit

--- GenericTrees/DisplayGT.java
+++ GenericTrees/DisplayGT.java
@@ -21,11 +21,7 @@ public static void display(Node node){
     }
 }
 
-
-
-
-
-    public static void main(String[]args) {
+public static void main(String[]args) {
         
         int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
         Node root = null;
